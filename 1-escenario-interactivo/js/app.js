e5ab3a38970f400db51e244ba1f218e1583e1b3f
dilanyz <velez123dilan@gmail.com>
2025-10-27 15:17:15 -0500

    maquetacion galeria

--- 1-escenario-interactivo/js/app.js
+++ 1-escenario-interactivo/js/app.js
@@ -1,10 +1,89 @@
-const botons = document.querySelectorAll("cambiarcolor");
-let colorOriginal = document.body.style.backgroundColor || 'red';
+/*captura variable */
+const score = document.querySelector('.score');
+const moneda = document.querySelectorAll('.coin');
 
-boton.addEventListener('click', function() {
-    if (document.body.style.backgroundColor === 'blue') {
-        document.body.style.backgroundColor = colorOriginal;
-    } else {
-        document.body.style.backgroundColor = 'blue';
-    }
-    });
+console.log(score);
+console.log(moneda);
+let i = 0;
+
+/*crear las funciones*/
+moneda.forEach( item => {
+    console.log("elemento: ", item);
+    
+    
+    item.addEventListener("click", () => {
+        item.classList.add("saltar")
+        i++
+        score.textContent = `Score: ${i}`
+ 
+    })
+
+    item.addEventListener("animationend", () => {
+        item.style.display = "none"
+   }, {once: true})     
+
+
+});
+
+
+
+
+/*captura variable */
+const jugadore = document.querySelectorAll('.tata');
+const scoren = document.querySelector('.score2');
+
+console.log(scoren);
+console.log(jugadore);
+let y = 0;
+
+
+
+/*crear las funciones*/
+jugadore.forEach( item => {
+    console.log("elemento: ", item);
+    
+    
+    item.addEventListener("click", () => {
+        item.classList.add("saltar")
+        y++
+        scoren.textContent = `Score: ${y}`
+ 
+    })
+
+    item.addEventListener("animationend", () => {
+        item.style.display = "none"
+   }, {once: true})     
+
+
+});
+
+/*captura variable */
+const score3 = document.querySelector('.score3');
+const gok = document.querySelectorAll('.gok');
+
+console.log(score3);
+console.log(gok);
+let k = 0;
+
+/*crear las funciones*/
+gok.forEach( item => {
+    console.log("elemento: ", item);
+    
+    
+    item.addEventListener("click", () => {
+        item.classList.add("saltar")
+        k++
+        score3.textContent = `Score: ${k}`
+ 
+    })
+
+    item.addEventListener("animationend", () => {
+        item.style.display = "none"
+   }, {once: true})     
+
+
+});
+
+
+
+
